8d43f06cf9d62d33cad62a78784bdedbcae26e3a
CMDY <caderinvader@outlook.com>
2025-05-07 20:16:39 -0400

    goofy changes test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/Encoders.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.drivetrain;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class Encoders {
+    private final DcMotorEx par, perp;
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/Localizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/Localizer.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
 public class Localizer {
-    private final DcMotorEx par, perp;
+    private final DcMotorEx parL, parR, perp;
     private final BNO055IMU imu;
 
     private double x = 0, y = 0, heading = 0;
@@ -17,18 +17,26 @@ public class Localizer {
     private int lastPar = 0, lastPerp = 0;
     private double lastHeading = 0;
 
+    private double parLDist = 0; // PAR POD DISTANCE FROM CENTER AXIS IN INCHES
+    private double parRDist = 0;
+    private double perpDist = 0; // PERP POD DISTANCE
+
     private static final double TICKS_PER_REV = 8192.0;
     private static final double WHEEL_RADIUS = 1.0; // inches
     private static final double GEAR_RATIO = 1.0;
     private static final double TICKS_TO_INCHES = 2 * Math.PI * WHEEL_RADIUS * GEAR_RATIO / TICKS_PER_REV;
 
     public Localizer(HardwareMap hardwareMap) {
-        par = hardwareMap.get(DcMotorEx.class, "par");
-        perp = hardwareMap.get(DcMotorEx.class, "perp");
+        parL = hardwareMap.get(DcMotorEx.class, "leftFront");
+        parR = hardwareMap.get(DcMotorEx.class, "rightFront");
+        perp = hardwareMap.get(DcMotorEx.class, "rightBack");
 
-        par.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        parL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        parR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         perp.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        par.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        parL.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        parR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         perp.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         imu = hardwareMap.get(BNO055IMU.class, "imu");
@@ -38,16 +46,17 @@ public class Localizer {
 
         lastHeading = getRawHeading();
     }
-
+    //  OLD CODE: double deltaPar = (parTicks - lastPar) * TICKS_TO_INCHES - (deltaHeading * parDist);
     public void update() {
-        int parTicks = par.getCurrentPosition();
+        int parLTicks = parL.getCurrentPosition();
+        int parRTicks = parR.getCurrentPosition();
         int perpTicks = perp.getCurrentPosition();
 
         double currentHeading = getRawHeading();
         double deltaHeading = angleWrap(currentHeading - lastHeading);
 
-        double deltaPar = (parTicks - lastPar) * TICKS_TO_INCHES;
-        double deltaPerp = (perpTicks - lastPerp) * TICKS_TO_INCHES;
+        double deltaPar = ((parRTicks * parLDist + parLTicks * parRDist)/(parRDist+parLDist));
+        double deltaPerp = (perpTicks - lastPerp) * TICKS_TO_INCHES - (deltaHeading * perpDist);
 
         lastPar = parTicks;
         lastPerp = perpTicks;
@@ -63,7 +72,7 @@ public class Localizer {
         y += dy;
         heading = currentHeading;
 
-        Pose2D robotPose = new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.RADIANS, 0);
+        Pose2D robotPose = new Pose2D(DistanceUnit.INCH, x, y, AngleUnit.RADIANS, heading);
     }
 
     public double getX() { return x; }
