5f7c869a42dfebec8d650e69e289884c062d88c5
cader <caderinvader@outlook.com>
2025-05-23 14:13:55 -0400

    idk if I pushed the last thing so this is a push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/demobotz/drive.java
+++
@@ -1,8 +0,0 @@
-// Code for the demo bots
-
-package org.firstinspires.ftc.teamcode.demobotz;
-
-public class drive {
-    @Override
-    public void runOpMode();
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrain/Action.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.teamcode.drivetrain;
-
-public interface Action {
-    void run();
-    boolean isComplete();
-}
